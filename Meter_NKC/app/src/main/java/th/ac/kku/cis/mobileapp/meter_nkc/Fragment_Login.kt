b25b23bf1c9565f2bb3e0a95d547dae97fa3c224
Zaksapthawee <i-am-zak_189@hotmail.com>
2020-03-26 01:24:59 +0700

    update_auth1.0
    importfirebase

---
+++ Meter_NKC/app/src/main/java/th/ac/kku/cis/mobileapp/meter_nkc/Fragment_Login.kt
@@ -0,0 +1,12 @@
+package th.ac.kku.cis.mobileapp.meter_nkc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fragment_Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_login)
+    }
+}
